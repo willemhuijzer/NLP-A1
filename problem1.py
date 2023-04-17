8218c5f95305b4bb22d612f44a0f6fa641500cd4
willemhuijzer <willemhuijzer2@hotmail.com>
2023-04-17 16:23:45 +0200

    Problem 1

--- problem1.py
+++ problem1.py
@@ -10,9 +10,19 @@ DO NOT SHARE/DISTRIBUTE SOLUTIONS WITHOUT THE INSTRUCTOR'S PERMISSION
 
 word_index_dict = {}
 
-# TODO: read brown_vocab_100.txt into word_index_dict
+# vf = vocab file in "r" mode (read mode)
+with open("brown_vocab_100.txt", "r") as vf:
+    word_index_dict = {}
 
-# TODO: write word_index_dict to word_to_index_100.txt
+    for index, line in enumerate(vf):
+        word = line.rstrip()
+        word_index_dict[word] = index
+
+# wf = word_to_index file in "w" mode (write mode)
+with open("word_to_index_100.txt", "w") as wf:
+    # Convert the dictionary to a string using the str() function
+    dict_str = str(word_index_dict)
+    wf.write(dict_str)
 
 
 
