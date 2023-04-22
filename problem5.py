dea9ef1113f63595abaa7c43593624d66a4656be
ElisedeKoning <g.e.de.koning@student.vu.nl>
2023-04-22 18:33:53 +0200

    add-alpha smoothing + calculation other probs

--- problem5.py
+++ problem5.py
@@ -28,10 +28,23 @@ for line in f:
         previous_word = word
 f.close()
 
-upper = counts_3[word_index_dict['in'], word_index_dict['the'], word_index_dict['past']]
-down = counts_2[word_index_dict['in'], word_index_dict['the']]
-probs = upper / down 
+# To calculate the trigrams, divide count(w1, w2, w3) by count(w1, w2)
+trigrams = [('in', 'the', 'past'), ('in', 'the', 'time'), ('the', 'jury', 'said'), ('the', 'jury', 'recommended'),
+            ('jury', 'said', 'that'), ('agriculture', 'teacher', ',')]
 
-print(probs)
-# Write the bigram probabilities to a file
-# print(probs[word_index_dict['in'], word_index_dict['the'], word_index_dict['past']])
+for trigram in trigrams:
+    word1, word2, word3 = trigram
+    numerator = counts_3[word_index_dict[word1], word_index_dict[word2], word_index_dict[word3]]
+    denominator = counts_2[word_index_dict[word1], word_index_dict[word2]]
+    trigram_probs = numerator / denominator
+    print(f"Trigram probability for {trigram}: {trigram_probs:.6f}")
+
+# Add alpha smoothing (cannot use Normalize function for counts_3 as it only accepts 2-dimensional arrays as input)
+# We use the following formula: p(w3 | w1, w2) = (count(w1, w2, w3) + alpha) / (count(w1, w2) + alpha * vocab)
+alpha = 0.1
+for trigram in trigrams:
+    word1, word2, word3 = trigram
+    numerator = counts_3[word_index_dict[word1], word_index_dict[word2], word_index_dict[word3]] + alpha
+    denominator = counts_2[word_index_dict[word1], word_index_dict[word2]] + alpha * len(word_index_dict)
+    trigram_probs = numerator / denominator
+    print(f"Smoothed trigram probability for {trigram}: {trigram_probs:.6f}")
